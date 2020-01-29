5255f37848aa094cc4a27ec51cfaab252e03aa0e
libin.bingo <libin.bingo@bytedance.com>
2020-01-29 17:34:07 +0800

    添加iOS的性能测试例子
    Change-Id: I26235332f25accd376c72bf1407e77c5231a89cd

---
+++ ios/performance/performance/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  performance
+//
+//  Created by libin on 2020/1/29.
+//  Copyright © 2020 cn.bingoli. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
